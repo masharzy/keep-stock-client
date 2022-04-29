24131392bdb54a96a0edf51d8b9338a7c01d2476
masharzy <horsnbd@gmail.com>
2022-04-29 20:49:02 +0600

    Started Designing

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './Pages/Home/Home/Home';
+import Header from './Pages/Shared/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
